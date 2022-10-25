c9f3b528289c0ff8a3842c70775adf9e4e08be7c
Danilo Barberini <danilo.barberini@fcamara.com.br>
2022-10-25 09:51:58 -0300

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setNome("Danilo");
+
+        System.out.printf(funcionario.getNome());
+    }
+}
